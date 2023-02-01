4f0c5e1ded23450fe839c1a4cd051863901a2870
danila bogdan <danilabogdan2018@gmail.com>
2023-02-01 14:01:14 +0200

    Navbar with logo and cart icon created/addeed

--- app.js
+++ app.js
@@ -1,10 +1,2 @@
 
-
-function change () {
-    var get = document.querySelector('.red');
-        get.classList.toggle('blue')
-}
-
-
-
-document.querySelector("button").addEventListener("click", change)
+ 
