70f29d3768f28d80d626f4aa82da559f57b169ed
danila bogdan <danilabogdan2018@gmail.com>
2023-02-02 11:58:43 +0200

    Implemented localStorage for the cart

--- app.js
+++ app.js
@@ -29,12 +29,15 @@
 
 //  console.log('shop');
 
-let basket = [];
+let basket = JSON.parse(localStorage.getItem("data")) || []
 
 
  let generateShop = () => {
     return (shop.innerHTML = shopItemsData.map((x)=> {
         let {id,name,price,desc,img} =x
+
+        let search = basket.find((x) => x.id === id) || []
+        
         return  `
         <div id=product-id-${id} class="item">
                 <img width="220" src=${img} alt="">
@@ -42,10 +45,11 @@ let basket = [];
                     <h3>${name}</h3>
                     <p>${desc}</p>
                     <div class="price-quantity">
-                        <h2>$ ${price}</h2>
+                        <h2>$${price}</h2>
                         <div class="buttons">
                             <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-                            <div id=${id} class="quantity">0</div>
+                            <div id=${id} class="quantity">
+                            ${search.item === undefined? 0: search.item}</div>
                             <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
                         </div>
                     </div>
@@ -76,6 +80,8 @@ let basket = [];
         search.item += 1;
     }
 
+    localStorage.setItem("data", JSON.stringify(basket));
+
     // console.log(basket);
     update(selectedItem.id);
  };
@@ -93,6 +99,9 @@ let basket = [];
     }
 
     // console.log(basket);
+
+    localStorage.setItem("data", JSON.stringify(basket));
+
     update(selectedItem.id);
  }
  let update = (id) => {
@@ -112,3 +121,6 @@ let basket = [];
 
     // console.log()
  };
+
+
+ calculation();
